f4a244d4a4648a91929bed810889652ca1a531dd
Divjotkaursaluja <divjot717@gmail.com>
2026-05-21 11:20:10 +0530

    AI prediction fixed

--- civicbackend/civic_backend/settings.py
+++ civicbackend/civic_backend/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'civic_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'civic_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- civicbackend/civic_backend/urls.py
+++ civicbackend/civic_backend/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/complaints/', include('complaints.urls')),   # correct
-    path('api/admin/', include('complaints.admin_urls')),  # correct
+    path('api/admin/', include('complaints.admin_urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
