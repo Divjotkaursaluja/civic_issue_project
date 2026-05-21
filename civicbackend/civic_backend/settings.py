21bfb9808ab6f5e7c41aba2a05c9e2d3f8b7c0d9
Divjotkaursaluja <divjot717@gmail.com>
2026-05-21 11:28:32 +0530

    AI prediction fix

--- civicbackend/civic_backend/settings.py
+++ civicbackend/civic_backend/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'civic_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'civic_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
